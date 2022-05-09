f8b1aada9c2ffe2d797e5ca7b71910d2e705d564
Martin Littlecott <mlittlec@hotmail.com>
2022-05-09 23:03:39 +0100

    Correct typo in restaurant.py

--- Dictionaries_and_Sets/restaurant.py
+++ Dictionaries_and_Sets/restaurant.py
@@ -11,7 +11,8 @@ def restaurant():
 
         if order in MENU:
             price = MENU[order]
-            total += priceprint(f'{order} is {price}, total is {total}.')
+            total += price
+            print(f'{order} is {price}, total is {total}.')
 
         else:
             print(f'We are fresh out of {order} today, sorry.')
